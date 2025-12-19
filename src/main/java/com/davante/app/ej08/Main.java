e9b023fc65ef33b13f78735dceeba6c098d9179a
Diego Ravese <diegoravese@gmail.com>
2025-12-19 16:53:58 +0100

    Ejercicio 08 realizado.

--- src/main/java/com/davante/app/ej08/Main.java
+++ src/main/java/com/davante/app/ej08/Main.java
@@ -1,9 +1,19 @@
 package com.davante.app.ej08;
 
+import java.util.Scanner;
+
 /*
 * 8. Convierte Celsius a Fahrenheit
 */
 
 public class Main {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce los grados Celsius");
+        double celsius = sc.nextInt();
+
+        double fahrenheit = (celsius * 1.8) + 32;
+        System.out.println(celsius + "º Celsius equivalen a " + fahrenheit + "º fahrenheit");
+        sc.close();
+    }
 }
