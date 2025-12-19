b4f0981758226c9651dbcb4ca36833ce81f3f48d
Diego Ravese <diegoravese@gmail.com>
2025-12-19 18:43:05 +0100

    Ejercicio 09 realizado

--- src/main/java/com/davante/app/ej09/Main.java
+++ src/main/java/com/davante/app/ej09/Main.java
@@ -6,6 +6,22 @@ package com.davante.app.ej09;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        int[] numeros = {10, 50, 20, 80, 30, 90, 40};
+
+        // 1. Asumimos que el primer elemento es el mas grande
+        int maximo = numeros[0];
+
+        // 2. Recorremos el array desde el segundo elemento (índice 1)
+        for ( int i = 1; i < numeros.length; i++ ) {
+            // 3. Comparamos el elemento actual con el máximo encontrado
+            if (numeros[i] > maximo) {
+                // 4. Si el actual es mayor, lo actualizamos como el nuevo máximo
+                maximo = numeros[i];
+            }
+        }
+
+        // 5. Imprimimos el resultado
+        System.out.println("El número más grande en el array es: " + maximo);
     }    
 }
