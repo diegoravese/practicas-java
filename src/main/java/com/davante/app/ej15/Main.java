ae01379f2ed62ac3b04f5b439383a1615c413df0
Diego Ravese <diegoravese@gmail.com>
2025-12-27 10:38:26 +0100

    Ejercicio 15 completado

--- src/main/java/com/davante/app/ej15/Main.java
+++ src/main/java/com/davante/app/ej15/Main.java
@@ -8,5 +8,13 @@ package com.davante.app.ej15;
  */
 
 public class Main {
-    
+    public static void main(String[] args) {
+        int cantidadFilas = 5;
+        for (int fila = 1; fila <= cantidadFilas; fila = fila + 1) {
+            for (int columna = 1; columna <= fila; columna = columna + 1) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
