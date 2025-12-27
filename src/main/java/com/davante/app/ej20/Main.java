83163808bf71e7541cb0626111db61e0f6bca816
Diego Ravese <diegoravese@gmail.com>
2025-12-27 11:28:30 +0100

    Ejercicio 20 completado

---
+++ src/main/java/com/davante/app/ej20/Main.java
@@ -0,0 +1,16 @@
+package com.davante.app.ej20;
+
+/**
+ *
+ * 20. Encuentra el área de un círculo
+ * @author diego
+ */
+public class Main {
+    public static void main(String[] args) {
+        double radio = 5.0;
+        double pi = 3.14159265;
+        double area = pi * radio * radio;
+        System.out.println("Radio: " + radio);
+        System.out.println("Área del círculo: " + area);
+    }
+}
--- src/main/java/com/davante/app/tema10/Main.java
+++ src/main/java/com/davante/app/tema10/Main.java
@@ -2,7 +2,7 @@ package com.davante.app.tema10;
 
 /**
  *
- * Tema 10
+ * Tema 10 - Herencia
  * @author diego
  */
 public class Main {
