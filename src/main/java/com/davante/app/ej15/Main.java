92a09e73f519280ea0afa1ee1172b951a01307ae
Diego Ravese <diegoravese@gmail.com>
2025-12-27 10:45:16 +0100

    Ejercicio 16 completado

--- src/main/java/com/davante/app/ej15/Main.java
+++ src/main/java/com/davante/app/ej15/Main.java
@@ -1,4 +1,3 @@
-
 package com.davante.app.ej15;
 
 /**
---
+++ src/main/java/com/davante/app/ej16/Main.java
@@ -0,0 +1,15 @@
+package com.davante.app.ej16;
+
+/**
+ *
+ * 16. Encuentra el valor ASCII de un caracter
+ * @author diego
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        char caracter = 'A';
+        int valorAscii = caracter;
+        System.out.println("El valor ASCII de " + caracter + " es " + valorAscii);
+    }
+}
